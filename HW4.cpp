b4077a13ae1d9252369794255f005e8bc6c1d913
dltmdcks07 <william070209@gmail.com>
2026-07-08 14:33:20 +0900

    Add files via upload

---
+++ HW4.cpp
@@ -0,0 +1,71 @@
+//#include <stdio.h>
+//void main()
+//{
+//	int i, k;
+//	for (i = 0; i < 3; i++)
+//	{
+//		for (k = 0; k < 2; k++)
+//		{
+//			printf("중첩 for문입니다.(i값: %d, k값: %d)\n", i, k);
+//		}
+//	}
+//}
+//#include <stdio.h>
+//void main()
+//{
+//	int i, k;
+//	for (i = 2; i <= 9; i++)
+//	{
+//		for(k=1; k <= 9; k++)
+//		{
+//			printf("%d * %d = %d\n", i, k, i * k);
+//		}
+//		printf("\n");
+//	}
+//}
+//#include <stdio.h>
+//void main()
+//{
+//	int i, k;
+//	for (i = 1; i <= 9; i++)
+//	{
+//		for(k=2; k<= 9; k++)
+//		{
+//			printf("%2d * %2d = %2d", k, i, k * i);
+//		}
+//		printf("\n");
+//	}
+//}
+//#include <stdio.h>
+//void main()
+//{
+//	int i, k;
+//	for (i = 1, k = 1; i <= 9; i++, k++)
+//		printf("%d * %d = %d\n", i, k, i * k);
+//}
+//#include <stdio.h>
+//void main()
+//{
+//	int i;
+//	i = 0;
+//	for (;;)
+//	{
+//		printf("%d \n", i);
+//		i++;
+//	}
+//}
+//#include <stdio.h>
+//void main()
+//{
+//	int i;
+//	for (i = 0; i = 128; i++)
+//	{
+//		if(i % 16 == 0)
+//		{
+//			printf("-------\n");
+//			printf("10진수 16진수 문자 \n");
+//			printf("-------\n");
+//		}
+//		printf("%5d %5x %5c \n", i, i, i);
+//	}
+//}
