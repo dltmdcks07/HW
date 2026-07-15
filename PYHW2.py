dbeba3d7a9d8d87dfd6cd609e7618ac0613fcf23
dltmdcks07 <william070209@gmail.com>
2026-07-15 12:29:13 +0900

    Add files via upload

---
+++ PYHW2.py
@@ -0,0 +1,45 @@
+##for i in range(5):
+##    print(str(i) + "=반복 변수")
+##print()
+##
+##for i in range(5,10):
+##    print(str(i) + "=반복 변수")
+##print()
+##
+##for i in range(0,10,3):
+##    print(str(i) + "=반복 변수")
+##print()
+
+##array = [273,32,103,57,52]
+##for i in range(len(array)):
+##    print("{}번째 반복: {}".format(i,array[i]))
+
+##for i in range(4,0-1,-1):
+##    print("현재 반복 변수: {}".format(i))
+
+##for i in reversed(range(5)):
+##    print("현재 반복 변수: {}".format(i))
+
+##output =""
+##for i in range(1,10):
+##    for i in range(0,i):
+##        output +="*"
+##    output +="\n"
+##print(output)
+
+##output =""
+##for i in range(1,15):
+##    for j in range(14,i,-1):
+##        output +=' '
+##    for k in range(0,2*i-1):
+##        output +='*'
+##    output +='\n'
+##print(output)
+
+##while True:
+##    print(".", end = "")
+
+##i=0
+##while i<10:
+##    print("{}번째 반복입니다.".format(i))
+##    i+=1
