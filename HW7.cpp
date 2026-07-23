bbac34d557a98e6ac47bd0be2abb7fe01cdf1112
dltmdcks07 <william070209@gmail.com>
2026-07-23 13:44:05 +0900

    Add files via upload

---
+++ HW7.cpp
@@ -0,0 +1,52 @@
+//#include <stdio.h>
+//void main()
+//{
+//	char ss[5] = "abcd";
+//	char tt[5];
+//	int i;
+//	for(i=0; i<4; i++)
+//	{
+//		tt[i] = ss[3-i];
+//	}
+//	tt[4] = '\0';
+//	printf("거꾸로 출력한 결과==>%s\n", tt);
+//}
+
+//#include <string.h>
+//#include <stdio.h>
+//void main()
+//{
+//	char ss[] = "XYZ";
+//	int len;
+//	len = strlen(ss);
+//	printf("문자열\"%s\"의 길이 ==> $d\n", ss, len);
+//}
+
+//#include <string.h>
+//#include <stdio.h>
+//void main()
+//{
+//	char ss[4];
+//	strcpy(ss, "XYZ");
+//	printf("문자열ss의 내용 ==> %s\n", ss);
+//}
+
+//#include <string.h>
+//#include <stdio.h>
+//void main()
+//{
+//	char ss[7] = "XYZ";
+//	strcat(ss, "ABC");
+//	printf("이어진 문자열ss의 내용 ==> %s\n", ss);
+//}
+
+//#include <string.h>
+//#include <stdio.h>
+//void main()
+//{
+//	char ss[] = "XYZ";
+//	char tt[] = "XYZ";
+//	int r;
+//	r = strcmp(ss, tt);
+//	printf("문자열의 비교 결과 ==> %d\n", r);
+//}
